3090b9887a3453fafe7241b1a4ebb53b230d1679
ygorp <ygor-nogueira@hotmail.com>
2022-04-26 22:58:10 -0300

    ...

---
+++ home/home.js
@@ -0,0 +1,21 @@
+import { criptografar, descriptografar } from "ncrypt-js";
+
+const ncrypt = require ("ncrypt-js");
+
+var textoInput = document.getElementById('texto');
+
+const e = 'enter';
+const i = 'imes';
+const a = 'ober';
+const o = 'ober';
+const u = 'ufat';
+
+function cripto(){
+  var 
+  const DadosCriptados = ncryptObject.criptografar(textoInput);
+
+  console.log("Texto simples: " + textoInput);
+
+}
+
+cripto();
