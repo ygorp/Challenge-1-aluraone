804b421fd622f525738945cdc6c3c51781d09772
ygorp <ygor-nogueira@hotmail.com>
2022-05-03 22:30:22 -0300

    configurando css e criando funcionalidade dos botôes

--- home/home.js
+++ home/home.js
@@ -1,5 +1,47 @@
+function formatarTexto(str) {
+  return str.normalize('NFD').replace(/[^a-zA-Z\s]/g, "");
+}
 
-var dados = document.getElementById("texto").value;
-var resultado = dados.replace(/e/g, 'enter', /i/g, 'imes', /a/g, 'ai', /o/g, 'ober', /u/g, 'ufat');
+function esconder() {
+  document.getElementById('esconder').style.display = 'none';
+  document.getElementById('codificado').style.display = 'block';
+  document.getElementById('copy').style.display = 'block';
 
-console.log(resultado);
+  document.getElementById('espaco').style.width = "800px";
+  document.getElementById('campo-cripto').style.marginLeft = "55%";
+}
+
+var cripto = document.getElementById('crypto').addEventListener('click', function(event) {
+
+  var dados = document.getElementById('text').value.toLowercase();
+
+  var codificar = dados.replace(/e/g, 'enter');
+  codificar = codificar.replace(/i/g, 'imes');
+  codificar = codificar.replace(/a/g, 'ai');
+  codificar = codificar.replace(/o/g, 'ober');
+  codificar = codificar.replace(/u/g, 'ufat');
+
+  document.getElementById('codificado').value = codificar;
+  document.getElementById('text').value = '';
+})
+
+var uncrypto = document.getElementById('uncrypto').addEventListener('click', function(event) {
+  var dados = document.getElementById('text').value;
+  var descodificar = dados.replace(/imes/g, 'i');
+  descodificar = descodificar.dados.replace(/enter/g, 'e');
+  descodificar = descodificar.dados.replace(/ai/g, 'a');
+  descodificar = descodificar.dados.replace(/ober/g, 'o');
+  descodificar = descodificar.dados.replace(/ufat/g, 'u');
+
+  document.getElementById('codificado').value = descodificar;
+  document.getElementById('text').value = '';
+})
+
+document.getElementById('copy').addEventListener('click', function() {
+
+  document.getElementById('codificado').ariaSelected();
+
+  document.execCommand('copy');
+
+  document.getElementById('codificado').value = '';
+})
